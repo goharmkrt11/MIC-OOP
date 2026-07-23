c2d9629ffb0e6eb69947c8f1bcb81cc1ef02e521
Gohar Mkrtchyan <Gohar Mkrtchyan@AB-PI49>
2026-07-23 17:09:53 +0400

    classlib separating change field into property

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,57 +1,71 @@
-﻿using System;
+﻿using ClassLib;
 
-
-namespace homework_1607
+namespace ChessConsole
 {
-    class Chess
+    internal class Program
     {
         static void Main()
         {
-            CreateMatrix();
-            ReadPosition();
-            ReadChessPiece();
-        }
-        public static void CreateMatrix()
-        {
-            char[,] board = new char[9, 9];
+            char[,] board = ChessBoard.CreateMatrix();
 
-            FillBoard(board);
-            FillLetters(board);
-            FillNumbers(board);
             PrintBoard(board);
-        }
-        static void FillNumbers(char[,] board)
-        {
-            for (int i = 0; i < 8; i++)
-            {
-                board[i, 0] = (char)('1' + i);
-            }
-        }
-        static void FillLetters(char[,] board)
-        {
-            char letter = 'A';
 
-            for (int i = 1; i < 9; i++)
-            {
-                board[8, i] = letter;
-                letter++;
-            }
-        }
-        static void FillBoard(char[,] board)
-        {
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 1; j < 9; j++)
-                {
-                    if ((i + j) % 2 == 0)
-                    {
-                        board[i, j] = '#';
-                    }
-                    else board[i, j] = '*';
-                }
-            }
+            Position position = ReadPosition();
+            ChessPiece piece = ReadChessPiece();
 
+            ChessBoard.PlacePiece(board, position, piece);
+
+            PrintBoard(board);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
+
+        static Position ReadPosition()
+        {
+            int row;
+
+            do
+            {
+                Console.WriteLine("Please enter a row (1-8):");
+            }
+            while (!int.TryParse(Console.ReadLine(), out row)
+                   || row < 1
+                   || row > 8);
+
+            char column;
+
+            do
+            {
+                Console.WriteLine("Please enter a column (A-H):");
+            }
+            while (!char.TryParse(
+                       (Console.ReadLine() ?? "").ToUpper(),
+                       out column)
+                   || column < 'A'
+                   || column > 'H');
+
+            return new Position(row, column);
+        }
+
+        static ChessPiece ReadChessPiece()
+        {
+            ChessPiece piece;
+
+            do
+            {
+                Console.WriteLine(
+                    "Please enter a chess piece (R, N, B, Q, K):");
+            }
+            while (!Enum.TryParse(
+                       (Console.ReadLine() ?? "").ToUpper(),
+                       out piece)
+                   || !Enum.IsDefined(piece));
+
+            return piece;
+        }
+
         static void PrintBoard(char[,] board)
         {
             for (int i = 0; i < 8; i++)
@@ -59,73 +73,27 @@ namespace homework_1607
                 for (int j = 0; j < 9; j++)
                 {
                     if (j == 0)
+                    {
                         Console.Write($"{board[i, j]}|");
+                    }
                     else
+                    {
                         Console.Write($"{board[i, j]} ");
+                    }
                 }
 
                 Console.WriteLine();
             }
 
             Console.WriteLine("  ________________");
+            Console.Write("  ");
 
-            for (int j = 0; j < 9; j++)
+            for (int j = 1; j < 9; j++)
             {
                 Console.Write($"{board[8, j]} ");
             }
-        }
-        struct Position
-        {
-            public int row;
-            public char column;
-            public Position(int row, char column)
-            {
-                this.row = row;
-                this.column = column;
-            }
-        }
-        static Position ReadPosition()
-        {
-            int row;
 
-            do
-            {
-                Console.WriteLine("Please enter a row(1-8):");
-            }
-            while (!int.TryParse(Console.ReadLine() , out row) || row<1 || row > 8);
-
-
-            char column;
-            do
-            {
-                Console.WriteLine("Please enter a column(A-H):");
-            }
-            while (!char.TryParse(Console.ReadLine().ToUpper(), out column) || column<'A' || column>'H');
-            return new Position(row,column);
+            Console.WriteLine();
         }
-        enum ChessPiece
-        {
-            R,//rook
-            N,//knight
-            B,//bishop
-            Q,//queen
-            K//king
-        }
-        static ChessPiece ReadChessPiece()
-        {
-            ChessPiece piece;
-            do
-            {
-                Console.WriteLine("Please enter a chess piece(R,N,B,Q,K):");
-            }
-            while (!Enum.TryParse(Console.ReadLine().ToUpper(), out piece));
-            return piece;
-        }
-
     }
-
 }
-
-
-
-        
---
+++ ClassLib/Class1.cs
@@ -0,0 +1,75 @@
+﻿namespace ClassLib
+{
+    public readonly struct Position
+    {
+        public int Row { get; set; }
+        public char Column { get; set; }
+
+        public Position(int row, char column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+
+    public enum ChessPiece
+    {
+        R,
+        N,
+        B,
+        Q,
+        K
+    }
+
+    public static class ChessBoard
+    {
+        public static char[,] CreateMatrix()
+        {
+            char[,] board = new char[9, 9];
+
+            FillBoard(board);
+            FillLetters(board);
+            FillNumbers(board);
+
+            return board;
+        }
+
+        private static void FillNumbers(char[,] board)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                board[i, 0] = (char)('1' + i);
+            }
+        }
+
+        private static void FillLetters(char[,] board)
+        {
+            char letter = 'A';
+
+            for (int i = 1; i < 9; i++)
+            {
+                board[8, i] = letter;
+                letter++;
+            }
+        }
+
+        private static void FillBoard(char[,] board)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    board[i, j] = (i + j) % 2 == 0 ? '#' : '*';
+                }
+            }
+        }
+
+        public static void PlacePiece(char[,] board, Position position, ChessPiece piece)
+        {
+            int row = position.Row - 1;
+            int column = position.Column - 'A' + 1;
+
+            board[row, column] = piece.ToString()[0];
+        }
+    }
+}
